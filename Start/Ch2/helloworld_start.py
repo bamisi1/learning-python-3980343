6ca00b47ff7d389b9878422ea408bf6788546f8e
bamisi1 <99725173+bamisi1@users.noreply.github.com>
2025-05-15 11:09:07 +0000

    updated exercise 01

--- Start/Ch2/helloworld_start.py
+++ Start/Ch2/helloworld_start.py
@@ -1,2 +1,5 @@
-# LinkedIn Learning Python course by Joe Marini
+# This is python learning joueny for Brian Amisi, join me.
 # Example file for HelloWorld
+print("Hello World")
+name = input("what is your name?") # this creates a variable name, with the input, when it runs, it asks to input name before proceeding to print
+print("nice to meet you", name)
