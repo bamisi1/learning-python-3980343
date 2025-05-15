61bdaa9def9d103c831f91d9755b8ee1a1229ca7
bamisi1 <99725173+bamisi1@users.noreply.github.com>
2025-05-15 14:48:55 +0000

    exercse learningi

--- Start/Ch2/sequence_start.py
+++ Start/Ch2/sequence_start.py
@@ -4,25 +4,44 @@
 # Sequences: Lists and Tuples
 # These are -- surprise -- sequences of values
 
+mylist =[0,1, "two", 3.2, False]
+# print(len(mylist))
+# print(mylist[2])
+# print(mylist[-1])
+# mylist[0]=10
+# print(mylist)
 
 # to access a member of a sequence type, use []
-
-
+#a string is also a list. ach character in a string is indexed. however, you cant change the char value or a string.
+mystr ="I am unbwogable"
+# print(mystr[0])
+#print(mystr)
 # add a list to another list
+mylist2 =[2,4,5,6,7]
 
+#print(mylist + mylist2)
 
 # use slices to get parts of a sequence
-
+#print(mylist2[::2])
 
 # you can use slices to reverse a sequence
+#print(mylist2[-1:-4:-1])
 
+# Tuples are like lists, but they are immutable () -hold data thta is not going to change
+mytuple = (1,2,3,4,5, "hey")
+#print(mytuple [2])
+#mytuple[2]=5
+#print(mytuple)# brings error because tuple are immutable.
 
-# Tuples are like lists, but they are immutable
-
-
-# Sets are also sequences, but they contain unique values
+# Sets are also sequences, but they contain unique values{}- enforce uniqueness in their membership.
+myset ={3,2,5,7,2, "hello"}
+#print(myset) #excludes printing the last 2, because it only allows one unique value (no duplication) 
 
 # Set, however, can not be indexed like lists or tuples
+
 # print(myset[0]) # this will cause an error
 
-# Test for membership
+# Test for membership  -- use 'in' to check if a value exists in a sequence
+print(3 in mylist2)
+print(4 in mylist2)
+print (7 in myset)
--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -1,3 +1,4 @@
+# This is python learning joueny for Brian Amisi, join me.
 # LinkedIn Learning Python course by Joe Marini
 # Example file for variables and basic types
 
@@ -11,11 +12,28 @@ mybool = True
 
 # We can display the content of a variable using the print() function
 
+# print(myint)
+# print(myfloat)
+# print(mybool)
 
 # Operators are used to perform operations on variables
 
+# print(myint + myfloat)
+# print(myint * myfloat)
+# print(myint / myfloat)
+# print(myint% 3)
+another_str = "This is another string" 
+
+# print(mystr + another_str)
+# print ("nom " *3)
+#you cant add diffent data types
 
 # Logical and comparison operators 
-
-
+# print(myint==10)
+# print(myfloat !=20)
+# print(myint>10)
+# print(myfloat < 20)
+#print(not (myfloat>26 and myint<15))
 # re-declaring a variable works
+myint = "this  redeclared"
+print(myint)
